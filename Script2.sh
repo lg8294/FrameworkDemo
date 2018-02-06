bcabf3cdaea0390d53a490870462d73efae799ad
arrcen-lg <515930926@qq.com>
2018-02-06 18:33:45 +0800

    no message

--- Script2.sh
+++ Script2.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-#  Script.sh
-#  ArrcenKit
+#  Script2.sh
+#  FrameworkDemo
 #
-#  Created by iOS on 16/9/23.
-#  Copyright © 2016年 iOS. All rights reserved.
+#  Created by iOS on 2018/2/6.
+#  Copyright © 2018年 lg. All rights reserved.
 
 # 如果工程名称和Framework的Target名称不一样的话，要自定义FMKNAME
 # 例如: FMK_NAME = "MyFramework"
@@ -27,8 +27,8 @@ SIMULATOR_RELEASE_DIR=${WRK_DIR}/Release-iphonesimulator/${FMK_NAME}.framework
 #SOCKET_SIMULATOR_RELEASE_DIR=${WRK_DIR}/Release-iphonesimulator/${SOCKET_FMK_NAME}.framework
 
 # 清理工程并编译
-xcodebuild -configuration "Release" -target "${FMK_NAME}" -sdk iphoneos -arch armv7 -arch armv7s -arch arm64 clean build
-xcodebuild -configuration "Release" -target "${FMK_NAME}" -sdk iphonesimulator -arch x86_64 -arch i386 clean build
+xcodebuild -configuration "Release" -target "${FMK_NAME}" -sdk iphoneos ONLY_ACTIVE_ARCH=NO clean build
+xcodebuild -configuration "Release" -target "${FMK_NAME}" -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO clean build
 
 # Cleaning the oldest.
 #删除自定义的输出文件夹
@@ -59,5 +59,4 @@ open "${SRCROOT}/Products/"
 
 #删除默认输出文件夹
 rm -r "${WRK_DIR}" #这里有时会由于库文件还没有合并完成就删除了原始库，导致合并失败
-
 
